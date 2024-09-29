b8545688d591e297e4791d6f7b73873b83540620
Josue Garcia <josue@sapphiretechglobal.com>
2024-09-28 23:55:09 -0600

    feat(eshop-backend): auth middleware
    Allowing non admins to update user profile without admin privileges

--- src/middlewares/roleMiddleware.ts
+++ src/middlewares/roleMiddleware.ts
@@ -7,9 +7,19 @@ export const requireAdmin = (
   next: NextFunction
 ) => {
   if (!req.user?.isAdmin) {
-    return res
-      .status(403)
-      .json({ success: false, message: "Access denied. Admins only." });
+    let errorMessage = "Access denied. Admins only.";
+
+    if (
+      req.method === "PATCH" &&
+      /^\/users\/\w+$/.test(req.path) &&
+      req.body.hasOwnProperty("isAdmin")
+    ) {
+      errorMessage = "Access denied. Only admins can grant privileges.";
+    } else {
+      next();
+    }
+
+    return res.status(403).json({ success: false, message: errorMessage });
   }
   next();
 };
